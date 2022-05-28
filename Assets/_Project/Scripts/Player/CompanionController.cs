e3432b6b1e93c5d1b3bd735f82f631e47e490cd1
Ahmet ÇINAR <32956741+ahmtcnr@users.noreply.github.com>
2022-05-28 10:58:35 +0300

    player movement

---
+++ Assets/_Project/Scripts/Player/CompanionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+
+public class CompanionController : MonoBehaviour
+{
+    [SerializeField] private float inputForce;
+
+    private Rigidbody2D rb;
+
+    private Vector2 inputVector;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
+
+
+        rb.AddForce(inputVector * inputForce, ForceMode2D.Force);
+    }
+}
---
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float inputX;
+    private bool jumpInput;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private LayerMask groundLayer;
+
+    [SerializeField] private Transform groundCheckTransform;
+    [SerializeField] private float groundCheckRadius;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+        jumpInput = Input.GetButton("Jump");
+
+        rb.velocity = new Vector2(inputX * moveSpeed, rb.velocity.y);
+
+        if (jumpInput && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (inputX != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(inputX), 1, 1);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheckTransform.position, groundCheckRadius, groundLayer);
+    }
+}
