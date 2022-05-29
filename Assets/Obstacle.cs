82a58aa1a91a5518a97b41f03a562c039235ca08
ging_safa <sdedeakay@yahoo.com>
2022-05-29 13:22:29 +0300

    feat: Add wavespawner

--- Assets/Obstacle.cs
+++ Assets/Obstacle.cs
@@ -1,9 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Obstacle : MonoBehaviour, IDamageable
 {
+    [SerializeField] private SpriteRenderer renderer;
+
+
+    private void Start()
+    {
+        renderer.color = Random.ColorHSV(0f, 1f, 0.4f, 0.8f, 0.6f, 1f);
+    }
+
     public int CurrentHealth { get; set; } = 100;
     public void Damage(int amount)
     {
--- Assets/_Project/Scripts/Managers/GameManager.cs
+++ Assets/_Project/Scripts/Managers/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    public PlayerController player;
 
     void Awake()
     {
